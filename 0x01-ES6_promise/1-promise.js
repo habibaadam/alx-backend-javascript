e168cca41aca7922ee61d5a14e3535bc55558bba
Habiba Adam <119977316+habibaadam@users.noreply.github.com>
2024-03-05 21:16:57 +0000

    fixing 1-promise

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,7 +1,10 @@
 export default function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
-    if (success === true) {
-      resolve({ status: 200, body: 'Sucess' });
+    if (success) {
+      resolve({
+        status: 200,
+        body: 'Success',
+      });
     } else {
       reject(new Error('The fake API is not working currently'));
     }
