9b46759b90f92780369cab6410e657ff0f220061
Habiba Adam <119977316+habibaadam@users.noreply.github.com>
2024-03-05 19:15:33 +0000

    3. Handle multiple successful promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,10 +1,10 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  const pic = uploadPhoto(); // returned promise saved within pic
-  const user = createUser(); // returned promise saved within user
-  let signUp = ''; // empty string
-  // iterating through the promises pic and user and adding the values to signUp
+  const pic = uploadPhoto();
+  const user = createUser();
+  let signUp = '';
+
   Promise.all([pic, user]).then((objects) => {
     for (const object of objects) {
       for (const [key, value] of Object.entries(object)) {
