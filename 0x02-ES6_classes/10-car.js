b9d9895e179b477a29661b873dfccfa2abca3bdd
Habiba Adam <119977316+habibaadam@users.noreply.github.com>
2024-03-07 12:39:01 +0000

    11. EVCar

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -5,7 +5,19 @@ export default class Car {
     this._color = color;
   }
 
+  get brand() {
+    return this._brand;
+  }
+
+  get motor() {
+    return this._motor;
+  }
+
+  get color() {
+    return this._color;
+  }
+
   cloneCar() {
-    return new this.constructor(this._brand, this._motor, this._color);
+    return new this.constructor();
   }
 }
