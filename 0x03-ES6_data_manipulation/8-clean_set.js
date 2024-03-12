825a530027577ef26d68c608513b79927a0327f9
Habiba Adam <119977316+habibaadam@users.noreply.github.com>
2024-03-12 10:22:45 +0000

    8. Clean set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,12 @@
 export default function cleanSet(theSet, startString) {
-  const filteredSet = Array.from(theSet)
-    .filter((item) => item.startsWith(startString))
-    .map((item) => item.substring(startString.length));
-
-  return filteredSet.join('-');
+  // checking if the beginning of string is not empty and is a string
+  if (!startString || !startString.length || typeof startString !== 'string') return '';
+  // empty string created to store resukt
+  let result = '';
+  // iterating through the set and slices the string from the startString length and appends '-'
+  theSet.forEach((item) => {
+    if (item && item.startsWith(startString)) result += `${item.slice(startString.length)}-`;
+  });
+  // returning the result but first removing the last '-' appended
+  return result.slice(0, result.length - 1);
 }
