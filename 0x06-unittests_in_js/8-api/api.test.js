d0c65f1fe1c2d51bef21926fdf476abc09bb56aa
Habiba Adam <119977316+habibaadam@users.noreply.github.com>
2024-05-02 11:31:53 +0000

    8. Basic Integration testing

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -3,14 +3,11 @@ const request = require('request');
 
 describe('Testing index page', () => {
   const url = 'http://localhost:7865';
-  it('Checks and test the main page status code', () => {
+  it('Checks and test the main page status code and body ', (done) => {
     request(url, (err, res, body) => {
       chai.expect(res.statusCode).to.equal(200);
-    })
-  })
-  it('Checks the main page content', () => {
-    request(url, (err, res, body) => {
       chai.expect(body).to.equal('Welcome to the payment system');
+      done();
     })
-  })
 })
+});
